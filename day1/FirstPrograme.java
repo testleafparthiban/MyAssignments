493cb8f9825f9c44aa6a1d80a9e4922665011e95
testleafparthiban <dparthiban760@gmail.com>
2024-11-15 14:54:20 +0530

    Add files via upload
    Day1

---
+++ day1/FirstPrograme.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class FirstPrograme {
+	public static void main(String[] args) {
+		System.out.println("This is my first programe");
+	}
+
+}
---
+++ day1/LearnIfElse.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+    
+		int num=0;
+		
+		
+		if (num>0) {
+			System.out.println("Positive number:"+num);
+			
+		}
+		else if (num==0) {
+			System.out.println("Number is netural:"+num);
+		}
+		else {
+			System.out.println("Negative numbr:"+num);
+		}
+	}
+
+}
